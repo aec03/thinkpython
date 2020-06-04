42f9190342143a68c99d5864fc2e09edb108bafb
aec03 <curtisaidan2@gmail.com>
2020-06-04 16:20:19 -0400

    added 3.4.py

--- chapter_3/3.3.py
+++ chapter_3/3.3.py
@@ -1,9 +1,7 @@
 # Write a function named right_justify that takes a string named s as a parameter and prints the string with enough leading spaces so that the last letter of the string is in column 70 of the display.
 
-
 def right_justify(s):
 # define fuction justify
-
     length = len(s)                 # get length of string
     spaces = 70 - length            # subtract length of string from 70
 
---
+++ chapter_3/3.4.py
@@ -0,0 +1,2 @@
+# A function object is a value you can assign to a variable or pass as an argument. For example, do_twice is a function that takes a function object as an argument and calls it twice
+
