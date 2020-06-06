cd09f167e939a9df7edf0ce7a9691d1a49d922aa
aec03 <curtisaidan2@gmail.com>
2020-06-06 09:46:45 -0400

    move variable position

--- chapter_4/4.3.py
+++ chapter_4/4.3.py
@@ -2,10 +2,6 @@
 from math import sin, cos, radians
 from swampy.TurtleWorld import TurtleWorld, Turtle, fd, lt, bk, rt
 
-world = TurtleWorld()
-bob = Turtle()
-
-
 def polygon_pie(t, length, sides):
 # draws polygon
     angle = (((sides - 2) * 180) / (sides * 2))         # gets interior angle of polygon and divides it by 2
@@ -19,6 +15,10 @@ def polygon_pie(t, length, sides):
         lt(t, (180 - angle))
         fd(t, slice)
         lt(t, 180)
+
+
+world = TurtleWorld()
+bob = Turtle()
 
 # call polygon_pie function to
 polygon_pie(bob, 100, 9)
