bad1345d9059de4c8cc67ad4884b8dc31525cee3
aec03 <curtisaidan2@gmail.com>
2020-06-06 14:32:41 -0400

    finish koch

---
+++ chapter_5/5.6_koch.py
@@ -0,0 +1,30 @@
+from swampy.TurtleWorld import *
+
+def koch(t, x):
+    """ draw koch curve """
+    if x < 4:
+        fd(t, x)
+    else:
+        koch(t, x / 4)
+        lt(t, 90)
+        koch(t, x / 4)
+        rt(t, 90)
+        koch(t, x / 4)
+        rt(t, 90)
+        koch(t, x / 4)
+        lt(t, 90)
+        koch(t, x / 4)
+
+
+
+def snowflake(t, x):
+    for _ in range(3):
+        koch(t, x)
+        rt(t, 120)
+    
+
+world = TurtleWorld()
+bob = Turtle()
+bob.delay = 0.01
+
+koch(bob, 100)
