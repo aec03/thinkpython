cc189557895a3de84e2d7714c383a34ee19999f3
aec03 <curtisaidan2@gmail.com>
2020-06-08 13:36:22 -0400

    fixed problem concerning repeated characters

--- chapter_10/10.7_list.py
+++ chapter_10/10.7_list.py
@@ -1,14 +1,20 @@
 # Write a function called is_anagram that takes two strings and returns True if they are anagrams.
 
 def is_anagram(s1, s2):
+    s1 = list(s1.lower())
+    s2 = list(s2.lower())
+
     if len(s1) != len(s2):
         return False
 
-    for i in s1.lower():
-        if i not in s2.lower():
+    s1.sort()
+    s2.sort()
+
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
             return False
 
     return True
 
 
-print(is_anagram('aaida', 'aidan'))
+print(is_anagram('nadin', 'aidan'))
